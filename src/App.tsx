7558f766b2fc6d5833d092b8cf817df2f050b153
a1dana04 <ajdanazumakmatova@gmail.com>
2024-07-01 22:38:48 +0600

    structura-stranisy

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,27 @@ import React from 'react';
 import './App.scss';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import { Route, Routes } from 'react-router-dom';
+import Vacancies from './components/Vacancies';
+import Events from './components/Events';
+import Video from './components/Video';
+import Organizations from './components/Organizations';
+import Community from './components/Community';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
         <Header/>
+
+        <Routes>
+        <Route path='/' element={<Home/>}/>
+          <Route path='/вакансии' element={<Vacancies/>}/>
+          <Route path='/мероприятия' element={<Events/>}/>
+          <Route path='/видео' element={<Video/>}/>
+          <Route path='/организации' element={<Organizations/>}/>
+          <Route path='/cooбщество' element={<Community/>}/>
+        </Routes>
     <Footer/>
   
     </div>
