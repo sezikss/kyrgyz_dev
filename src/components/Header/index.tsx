fceb5e706d88b21157cb22b8adb118683b0a4ab7
a1dana04 <ajdanazumakmatova@gmail.com>
2024-07-10 12:10:26 +0600

    header stranisalaryn tysy black

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,7 @@ import logoHeader from "../assets/img/Logo-header.svg";
 import logoHeader1 from "../assets/img/Logo-header.svg";
 import { RiLoginCircleLine } from "react-icons/ri";
 import { TfiMenu } from "react-icons/tfi";
-import { Link } from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
 
 const Header: React.FC = () => {
   const [isModalOpen, setModalOpen] = useState<boolean>(false);
@@ -15,33 +14,31 @@ const Header: React.FC = () => {
         <div className="header">
           <img src={logoHeader} alt="img" />
           <div className="header--nav">
-            <Link to={"/вакансии"}>Вакансии</Link>
-            <Link to={"/мероприятия"}>Мероприятия</Link>
-            <Link to={"/видео"}>Видео</Link>
-            <Link to={"/организации"}>Организации</Link>
-            <Link to={"/cooбщество"}>Сообщество</Link>
+            <NavLink to="/вакансии" className={({ isActive }) => isActive ? "active" : ""}>Вакансии</NavLink>
+            <NavLink to="/мероприятия" className={({ isActive }) => isActive ? "active" : ""}>Мероприятия</NavLink>
+            <NavLink to="/видео" className={({ isActive }) => isActive ? "active" : ""}>Видео</NavLink>
+            <NavLink to="/организации" className={({ isActive }) => isActive ? "active" : ""}>Организации</NavLink>
+            <NavLink to="/cooбщество" className={({ isActive }) => isActive ? "active" : ""}>Сообщество</NavLink>
           </div>
 
-
-        <h1 onClick={() => setModalOpen(true)}><TfiMenu /></h1> 
-      {isModalOpen && (
-        <div className="header--modal">
-          <div className="header--modal__content">
-         <div className="header--modal__content--img">
-         <img src={logoHeader1} alt="img" style={{
-          width:"90px"
-         }} />
-          <span onClick={() => setModalOpen(false)}>x</span>
-         </div>
-         <Link to={"/вакансии"}>Вакансии</Link>
-            <Link to={"/мероприятия"}>Мероприятия</Link>
-            <Link to={"/видео"}>Видео</Link>
-            <Link to={"/организации"}>Организации</Link>
-            <Link to={"/cooбщество"}>Сообщество</Link>
-            
-          </div>
-        </div>
-      )}
+          <h1 onClick={() => setModalOpen(true)}><TfiMenu /></h1>
+          {isModalOpen && (
+            <div className="header--modal">
+              <div className="header--modal__content">
+                <div className="header--modal__content--img">
+                  <img src={logoHeader1} alt="img" style={{
+                    width: "90px"
+                  }} />
+                  <span onClick={() => setModalOpen(false)}>x</span>
+                </div>
+                <NavLink to="/вакансии" className={({ isActive }) => isActive ? "active" : ""}>Вакансии</NavLink>
+                <NavLink to="/мероприятия" className={({ isActive }) => isActive ? "active" : ""}>Мероприятия</NavLink>
+                <NavLink to="/видео" className={({ isActive }) => isActive ? "active" : ""}>Видео</NavLink>
+                <NavLink to="/организации" className={({ isActive }) => isActive ? "active" : ""}>Организации</NavLink>
+                <NavLink to="/cooбщество" className={({ isActive }) => isActive ? "active" : ""}>Сообщество</NavLink>
+              </div>
+            </div>
+          )}
           <div className="header--login">
             <div className="header--login__btn">
               <span>
@@ -54,10 +51,8 @@ const Header: React.FC = () => {
           </div>
         </div>
       </div>
-    
     </div>
   );
 };
 
 export default Header;
-
