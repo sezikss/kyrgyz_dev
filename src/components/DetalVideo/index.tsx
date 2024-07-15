64e916c3f94e1fddde0a0b4a4a68128a4fa28965
a1dana04 <ajdanazumakmatova@gmail.com>
2024-07-16 00:23:28 +0600

    bylo izmeneno detalnyi sait и sdelala adaptiku

--- src/components/DetalVideo/index.tsx
+++ src/components/DetalVideo/index.tsx
@@ -1,68 +1,76 @@
 import React from "react";
 import detal1 from "../../components/assets/img/video1.webp";
 
-const DetalVideo = () => {
+const VideoDetail = () => {
   return (
-    <div>
-      <div className="detalvideo-container">
+    <>
+      <div id="videoDetailTitle">
         <div
-          id="detalvideo"
-          className="background"
+          className="videoDetailTitle--img"
           style={{
             backgroundImage: `url(${detal1})`,
-        
-     
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
           }}
-        />
-        <div className="content">
-          <h1>
-            ChatGPT — революция или мода? Как <br /> нейросети могут помочь в
-            работе
-          </h1>
-          <div className="details">
-            <div className="detail">
-              <h6>Организатор</h6>
-              <h3>Kolesa Group</h3>
-            </div>
-            <div className="detail">
-              <h6>Когда</h6>
-              <h3>12 сентябрь 2023</h3>
+        ></div>
+        <div className="container videoDetail">
+          <div className="videoDetailTitle">
+            <h1>
+              ChatGPT — революция или мода? Как <br />
+              нейросети могут помочь в работе
+            </h1>
+            <div className="videoDetailTitle--date">
+              <div className="videoDetailTitle--date__block">
+                <h4>Когда</h4>
+                <h3>12 сентября 2023</h3>
+              </div>
+              <div className="videoDetailTitle--date__block">
+                <h4>Организатор</h4>
+                <h3>Kolesa Group</h3>
+              </div>
             </div>
           </div>
-          <div className="video-container">
+        </div>
+      </div>
+      <div id="video-content">
+        <div className="container">
+          <div className="video-content">
             <iframe
-              width="1250"
-              height="450"
+              width="100%"
+              height="585"
               src="https://www.youtube.com/embed/C2ICic8y5D8"
               title="ChatGPT — революция или мода? Как нейросети могут помочь в работе. Kolesa Podcast"
-              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowFullScreen
             ></iframe>
           </div>
         </div>
       </div>
-      <div className="container">
-        <p>
-          Завершаем пятый сезон нашумевшей темой про генеративные модели. Что
-          обсуждали:
-        </p>
-
-        <p>
-          - как правильно применять ChatGPT в своей работе
-          <br />
-          - влияние широкого распространения нейросетей
-          <br />- бизнес-риски применения искусственного интеллекта
-        </p>
-
-        <p>
-          В гостях были: <br />
-          · Хамбар Дусалиев — Head of ML & Operations at Kolesa Group <br />·
-          Камалхан Артыкбаев — Lead Computer vision Engineer at Parqour
-        </p>
+      <div id="video-down">
+        <div className="container">
+          <div className="video-down">
+            <button>Перейти на YouTube</button>
+            <div className="video-down--text">
+              <p>
+                Завершаем пятый сезон нашумевшей темой про генеративные модели.
+                Что обсуждали:
+              </p>
+              <p>
+                - как правильно применять ChatGPT в своей работе <br /> -
+                влияние широкого распространения нейросетей <br />- бизнес-риски
+                применения искусственного интеллекта
+              </p>
+              <p>
+                В гостях были: <br />· Хамбар Дусалиев — Head of ML & Operations at
+                Kolesa Group <br /> · Камалхан Артыкбаев — Lead Computer vision
+                Engineer at Parqour
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
-export default DetalVideo;
+export default VideoDetail;
