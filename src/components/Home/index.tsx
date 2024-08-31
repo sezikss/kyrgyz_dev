36e7abec3bcbd20fae0091fbcd324c43efda57c3
Lenovo <talantbekovasezimai@github.com>
2024-08-31 15:59:04 +0600

    add video UI

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Events from '../Events';
-import Vacancies from '../Vacancies';
 import Video from '../Video';
+import Vacancies from '../Vacancies';
 
 const Home = () => {
     return (
@@ -11,7 +11,7 @@ const Home = () => {
                 <h1>Последние мероприятия</h1>
                 <Events count={1}/>
                 <h1>Последние вакансии</h1>
-                <Vacancies count={4} />
+                <Vacancies count={4}/>
                 <h1>Последние видео</h1>
                 <Video count={2}/>     
             </div>
--- src/components/Vacancies/VacancyList.tsx
+++ src/components/Vacancies/VacancyList.tsx
@@ -1,16 +1,6 @@
 import { Link } from "react-router-dom";
 
-export const VacancyList = ({
-  companyName = "",
-  jobTitle = "",
-  paymentType = "",
-  priceFrom = 0,
-  priceTo = 0,
-  type = "",
-  city = "",
-  currency = "",
-  salary = "",
-}: {
+type VacancyListProps = {
   companyName: string;
   jobTitle: string;
   paymentType: string;
@@ -20,29 +10,43 @@ export const VacancyList = ({
   city: string;
   currency: string;
   salary: string;
+};
+
+const VacancyList: React.FC<VacancyListProps> = ({
+  companyName,
+  jobTitle,
+  paymentType,
+  priceFrom,
+  priceTo,
+  type,
+  city,
+  currency,
+  salary,
 }) => {
   return (
-<div className="cards">
-<Link to={"/detailvacancy"}>
-  <div className="blocks">
-    <div className="company">
-      <p>Компания</p>
-      <h3> {companyName}</h3>
+    <div className="cards">
+      <Link to={"/detailvacancy"}>
+        <div className="blocks">
+          <div className="company">
+            <p>Компания</p>
+            <h3>{companyName}</h3>
+          </div>
+          <div className="dol">
+            <p>Должность</p>
+            <h3>{jobTitle}</h3>
+          </div>
+          <div className="oclad">
+            <p>Оклад</p>
+            <h3>{`${priceFrom} - ${priceTo} ${currency} в ${paymentType}`}</h3>
+          </div>
+          <div className="office">
+            <p>Тип</p>
+            <h3>{`${type}/ ${city}`}</h3>
+          </div>
+        </div>
+      </Link>
     </div>
-    <div className="dol">
-      <p>Должность</p>
-      <h3>{jobTitle}</h3>
-    </div>
-    <div className="oclad">
-      <p>оклад</p>
-      <h3>{`${priceFrom}- ${priceTo} ${currency} в ${paymentType}`}</h3>
-    </div>
-    <div className="office">
-      <p>тип</p>
-      <h3>{`${type}/ ${city}`}</h3>
-    </div>
-  </div>
-</Link>
-</div>
-);
+  );
 };
+
+export default VacancyList;
